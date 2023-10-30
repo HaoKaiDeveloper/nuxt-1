ea9b5712d38bafa145eeb1328a8f2493ab69f9c8
haokai <haokai@gmail.com>
2023-10-30 14:09:52 +0800

    upload

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,19 @@
+<!-- 
+這條路由是/car/[name]-[id],
+其中[name]跟[id]都是動態的，
+如果你是[name]、[id]那他們都是必填項目，
+/car/toyota-5之類的，
+-->
+<template>
+  <Navbar />
+  <div
+    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+  >
+    <div>
+      <CarDetailHero />
+    </div>
+    <CarDetailAttributes />
+    <CarDetailDescription />
+    <CarDetailContact />
+  </div>
+</template>
---
+++ pages/city/[city]/car/[[make]].vue
@@ -0,0 +1,22 @@
+<!-- /city/:city/car/:make
+    /city是固定的,
+    /city/:city，其中的:city是動態，如果需要接續/car那就是必填的,
+    /car是固定的,
+    /:make，看起來是動態的，但他的檔案結構是[[make]]].vue，這樣代表可以填也可以不填，是非必要填的路由
+    也就是說這條路徑，
+    /city/abcd/car可以
+    /city/abcd/car/2345也可以
+     -->
+<template>
+  <div>
+    <Navbar />
+    <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+      <div class="mt-32 flex">
+        <CarSideBar />
+        <CarCards />
+      </div>
+    </div>
+  </div>
+</template>
